b51c7a7d457bc70268dfa036008026a1922ba863
Leonardo Oliveira <lolive4@sibsnet.local>
2021-11-04 16:43:34 +0000

    Class done, all thats left is connect it to the RecyclerView

--- app/src/main/java/com/example/leoandroidportfolio/MainActivity.java
+++ app/src/main/java/com/example/leoandroidportfolio/MainActivity.java
@@ -3,14 +3,35 @@ package com.example.leoandroidportfolio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titleText;
+    TextView descriptionText;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViews();
+
+        Project[] projects = {
+                new Project("BMI Calculator","To calculate peoples ideal weight.", R.drawable.calculadora),
+                new Project("Calculation App","From Inches to Meters.", R.drawable.einstein),
+                new Project("Francesinhas","A menu app for Francesinhas", R.drawable.francesinha),
+                new Project("I Buy It","Online Market", R.drawable.market_online)
+
+        };
+    }
+
+    private void findViews() {
+        titleText = findViewById(R.id.text_view_project_title);
+        descriptionText = findViewById(R.id.text_view_project_description);
+        imageView = findViewById(R.id.image_view_project_icon);
 
     }
 }
--- app/src/main/java/com/example/leoandroidportfolio/Project.java
+++ app/src/main/java/com/example/leoandroidportfolio/Project.java
@@ -1,4 +1,21 @@
 package com.example.leoandroidportfolio;
 
 public class Project {
+
+    // State
+    String name;
+    String description;
+    int image;
+    // R.drawable.name_of_the_file (This will be some sort of number, an 'int')
+    // e.g. 15238475
+
+    // Behavior
+
+
+    // Contructor
+    public Project(String name, String description, int image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
 }
